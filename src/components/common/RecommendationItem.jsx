303836ddc7dd20061304ed194aa56af8f255d969
danalee <eleeje97@gmail.com>
2022-09-20 12:13:34 +0900

    recommendation css 수정

--- src/components/common/RecommendationItem.jsx
+++ src/components/common/RecommendationItem.jsx
@@ -5,7 +5,7 @@ function RecommendationItem(img_src) {
   return (
     <div className="col">
       <div href="#" className="card-img-top card-img-bottom">
-        <img class="card-img-top" src={img_src.img_src} alt="Card image cap" />
+        <img class="card-img-top reco-img-size" src={img_src.img_src} alt="Card image cap" />
           <div className="btn demo-inline-spacing" >
             <Button text="피팅룸으로 가기" path={"/fittingroom"}/>
           </div>
