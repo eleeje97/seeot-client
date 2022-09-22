469e4fdbb45ba105a962c696b9ae83218cbdd59f
danalee <eleeje97@gmail.com>
2022-09-22 22:43:59 +0900

    [Fitting Room] /clothes/list API 연결, 렌더

--- src/page/FittingRoom.jsx
+++ src/page/FittingRoom.jsx
@@ -23,6 +23,8 @@ function FittingRoom() {
     const [seeotClothesState, setSeeotClothesState] = useState(false);
     const [fittingModalOpen, setFittingModalOpen] = useState(false);
     const [uploadModalOpen, setUploadModalOpen] = useState(false);
+    const [myClothes, setMyClothes] = useState([]);
+    const [seeotClothes, setSeeotClothes] = useState([]);
 
     const openFittingModal = () => {
         setFittingModalOpen(true);
@@ -50,7 +52,8 @@ function FittingRoom() {
                 .clothesList(userId)
                 .then((res) => {
                     if (res.status === 200) {
-                        console.log('userclothes ' + JSON.stringify(res.data));
+                        setMyClothes(res.data.user_clothes);
+                        setSeeotClothes(res.data.recommend_clothes);
                     }
                 })
                 .catch(function (e) {
@@ -103,7 +106,7 @@ function FittingRoom() {
             <Logo />
             <div className="container-xxl text-end">
                 <button className="btn btn-outline-primary" onClick={openUploadModal}><BiUpload /> Upload</button>
-                <UploadModal openState={uploadModalOpen} close={closeUploadModal} userInfo={user}/>
+                <UploadModal openState={uploadModalOpen} close={closeUploadModal} userInfo={user} />
                 <div className={uploadModalOpen ? "modal-backdrop fade show" : ""}></div>
             </div>
             <div className="container-xxl flex-grow-1 container-p-y">
@@ -158,20 +161,20 @@ function FittingRoom() {
                             <div className={myClothesState ? "tab-pane fade d-flex show active" : "tab-pane fade"} id="navs-pills-justified-home" role="tabpanel">
                                 <div className="container-xxl flex-grow-1 container-p-y">
                                     <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
-                                        <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
-                                            closeModal={closeFittingModal} img_src={First} />
-                                        <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
-                                            closeModal={closeFittingModal} img_src={Second} />
-                                        <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
-                                            closeModal={closeFittingModal} img_src={Third} />
+                                        {myClothes.map((img_url) => (
+                                            <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
+                                                closeModal={closeFittingModal} img_src={'http://210.106.99.80:5050/' + img_url} />
+                                        ))}
                                     </div>
                                 </div>
                             </div>
                             <div className={seeotClothesState ? "tab-pane fade d-flex show active" : "tab-pane fade"} id="navs-pills-justified-profile" role="tabpanel">
                                 <div className="container-xxl flex-grow-1 container-p-y">
                                     <div className="row row-cols-1 row-cols-md-3 g-4 mb-5">
-                                        <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
-                                            closeModal={closeFittingModal} img_src={Third} />
+                                        {seeotClothes.map((img_url) => (
+                                            <Clothes modalOpen={fittingModalOpen} openModal={openFittingModal}
+                                                closeModal={closeFittingModal} img_src={'http://210.106.99.80:5050/' + img_url} />
+                                        ))}
                                     </div>
                                 </div>
                             </div>
