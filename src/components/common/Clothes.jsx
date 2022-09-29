5a05d5d63251ea31ea38ad5fd576b1bc0c81ccc8
Admin <dlatpwls524@gmail.com>
2022-09-30 04:36:03 +0900

    [Fitting Room] 상의/하의 체크 border css

--- src/components/common/Clothes.jsx
+++ src/components/common/Clothes.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import FittingModal from "./FittingModal"
 
-function Clothes({ modalOpen, openModal, closeModal, img_src, topCheck, bottomCheck, onClick, clothes_id }) {
+function Clothes({ border, modalOpen, openModal, closeModal, img_src, topCheck, bottomCheck, onClick, clothes_id }) {
   const [id, setId] = useState(clothes_id);
 
   return (
@@ -12,7 +12,9 @@ function Clothes({ modalOpen, openModal, closeModal, img_src, topCheck, bottomCh
 
       <div className="col">
         <div href="#" className="card-img-top card-img-bottom">
-          <img className="card-img-top card-img-bottom " src={img_src} alt="Card image cap"
+          <img className={border === 'top' ? "card-img-top card-img-bottom img-top-border" : 
+                            border === 'bottom' ? "card-img-top card-img-bottom img-bottom-border" : "card-img-top card-img-bottom"}
+               src={img_src} alt="Card image cap"
             onClick={() => {
               openModal(clothes_id);
               setId(clothes_id);
